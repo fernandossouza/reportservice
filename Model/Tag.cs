1ca2eb32b2c7b8a296278f10f1b86bd6127e8485
fernandossouza <fernandosluno@gmail.com>
2018-04-06 16:46:24 -0300

    Add field color

--- Model/Tag.cs
+++ Model/Tag.cs
@@ -5,6 +5,7 @@ namespace reportservice.Model
     public class Tag
     {
         public string name{get;set;}
+        public string color{get;set;}
         public string group{get;set;}
         public List<long> timestamp {get;set;}
         public List<string> value{get;set;}
